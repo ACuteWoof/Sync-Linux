3e0f96bced4c000db4c520f80b68086ff3f07490
ACuteWoof <63534975+ACuteWoof@users.noreply.github.com>
2021-03-04 14:10:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import os
 
+password = input('password (leave blank if none): ')
+os.system('sudo snap install --classic code') #for distros with snap store
+os.system('sudo apt install gdebi-core')
+os.system('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb')
+os.system('sudo apt install ./google-chrome-stable_current_amd64.deb')
+os.system('wget ~/minecraft.deb https://launcher.mojang.com/download/Minecraft.deb')
+os.system('sudo gdebi ~/minecraft.deb')
